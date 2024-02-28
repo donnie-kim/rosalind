34936b98a20543a2a92281a2ef02e14bb26c6359
Dong-Hyun <kimdh8686@gmail.com>
2024-02-27 18:02:04 -0600

    Mortal fib

---
+++ Bioinformatics Stronghold/mortal-fibonacci-rabbits.py
@@ -0,0 +1,16 @@
+def mortal_fib_rabbits(n, m):
+    pairs = [0] * n
+    pairs[0] = 1
+    pairs[1] = 1
+
+    for i in range(2, n):
+        if i < m:
+            pairs[i] = pairs[i - 1] + pairs[i - 2]
+        elif i == m or i == m + 1:
+            pairs[i] = pairs[i - 1] + pairs[i - 2] - 1
+        else:
+            pairs[i] = pairs[i - 1] + pairs[i - 2] - pairs[i - (m + 1)]
+    return pairs[-1]
+
+
+print(mortal_fib_rabbits(91, 18))
