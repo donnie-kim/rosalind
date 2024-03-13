27b6c99f4df9dfff07f549c1e8b37281b716c255
Dong-Hyun <kimdh8686@gmail.com>
2024-03-12 18:55:06 -0600

    13-Calculating Expected Offspring

---
+++ Bioinformatics Stronghold/13-IEV.py
@@ -0,0 +1,34 @@
+# Calculating Expected Offspring
+"""
+1. AA-AA p = 1.0
+2. AA-Aa p = 1.0
+3. AA-aa p = 1.0
+4. Aa-Aa p = 0.75
+5. Aa-aa p = 0.50
+6. aa-aa p = 0.0
+"""
+
+
+def read_populations(filename):
+    file = open(filename, "r")
+    content = file.read().strip()
+    populations = content.split()
+    populations = [int(num) for num in populations]
+    # print(populations)
+    return populations
+
+
+def expected_offspring(populations, n):
+    E1 = populations[0] * 1 * 2
+    E2 = populations[1] * 1 * 2
+    E3 = populations[2] * 1 * 2
+    E4 = populations[3] * 0.75 * 2
+    E5 = populations[4] * 0.50 * 2
+    E6 = populations[5] * 0 * 2
+    total = E1 + E2 + E3 + E4 + E5 + E6
+    return total
+
+
+# print(read_populations("test.txt"))
+populations = read_populations("rosalind_iev.txt")
+print(expected_offspring(populations, 2))
